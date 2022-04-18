0de9d27c4d3351f9d9d05c81427b7647d64bf7e6
ali konuk <alikonuk1@gmail.com>
2022-04-18 15:46:28 +0300

    some changes

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+/**
+ * @title SimpleStorage
+ * @dev Store & retrieve value in a variable
+ */
 contract SimpleStorage {
   uint storedData;
 
+  /**
+  * @dev Store value in variable
+  * @param x value to store
+  */
   function set(uint x) public payable {
     storedData = x;
   }
 
+  /**
+  * @dev Return value
+  * @return value of 'storedData'
+  */
   function get() public view returns (uint) {
     return storedData;
   }
